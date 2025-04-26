b6ed6afe386df907006d3be2dc634a601b6e8d4c
Renan Andrade <77841497+Laggh@users.noreply.github.com>
2025-04-26 14:52:18 -0300

    Update index.js

--- server/index.js
+++ server/index.js
@@ -37,7 +37,7 @@ app.use((req, res, next) => {
 // });
 
 app.get("/", (req,res) => {
-    res.json({message: "Hello World"});
+    res.json({message: "DS é o TERROR"});
 });
 
 // app.post("/", (req,res) => {
