01c9dd22f14f53cd3cf77fbb19989d30f9e29c4c
Renan Andrade <77841497+Laggh@users.noreply.github.com>
2025-04-26 16:08:15 -0300

    Melhora da estrutura do codigo do server/

--- server/index.js
+++ server/index.js
@@ -1,32 +1,41 @@
+//Funcionalidade principal
 const express = require("express");
 const mysql = require("mysql2");
+
+//Coisas do Express
 const bodyParser = require("body-parser");
 const cors = require("cors");
 
+//Configuração de mini-bibliotecas
 const dotenv = require("dotenv");
 dotenv.config();
 
+//Constantes
 const PORT = process.env.PORT || 10000 
 
+//Inicialização do servidor
 const app = express();
-
 app.use(bodyParser.json()); 
 app.use(cors()); 
 
-
+// Criação do pool de conexões com o BD
+// (pool é um conjunto de conexões que podem ser reutilizadas,
+// o que melhora a performance do servidor e não precisa ficar
+// criando e destruindo conexões toda hora)
 const pool = mysql.createPool({
+    // Configurações do banco de dados usando as variaveis de ambiente
     host: process.env.DB_HOST,
-    port: Number(process.env.DB_PORT), // Convert port to a number
+    port: Number(process.env.DB_PORT), 
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME,
 
-    waitForConnections: true,
-    connectionLimit: 10,
-    queueLimit: 0    
+    waitForConnections: true, // Espera por conexões evitando criar novas
+    connectionLimit: 10, // Limite de conexões simultâneas
+    queueLimit: 0 // Sem limite de filas de espera
 });
     
-
+// Printa no console o IP, método e URL de cada requisição
 app.use((req, res, next) => {
     const ip = req.ip || req.connection.remoteAddress;  // Get the IP address
     const method = req.method;
@@ -37,14 +46,10 @@ app.use((req, res, next) => {
     next();
 });
 
-  
-// db.connect((err) => {
-//     if (err) {
-//         console.log("erro de conexão com o banco:",err);
-//         return
-//     }
-//     console.log("Connected to MySQL");
-// });
+app.get("/ping", (req, res) => {
+    res.json({ message: "Pong!" });
+});
+
 
 app.get("/", (req,res) => {
     pool.query("SELECT * FROM teste", (err, results) => {
@@ -57,49 +62,7 @@ app.get("/", (req,res) => {
     });
 });
 
-// app.post("/", (req,res) => {
-//     //const {name, message} = req.body
-//     const name = req.body.name
-//     const message = req.body.message
-    
-//     console.log(req.body);
-//     if(!name || !message){
-//         res.status(400).json({error: "Name e Message são obrigatorios"});
-//         return
-//     }
-
-//     db.query("INSERT INTO messages(name,message) VALUES (?,?)", [name,message], (err,results) =>{
-//         if (err) {
-//             res.status(500).json({ error: err.message });
-//             return;
-//         }
-        
-//         const resposta = {
-//             id: results.insertId,
-//             name: name,
-//             message: message
-//         }
-//         res.json(resposta);
-//     });
-
-// });
-
-// app.delete('/:id', (req, res) => {
-//     const id = req.params.id;
-
-//     db.query('DELETE FROM messages WHERE id = ?', [id], (err, result) => {
-//         if (err) {
-//             res.status(500).json({ error: err.message });
-//             return;
-//         }
-//         if (result.affectedRows === 0) {
-//             return res.status(404).json({ error: 'Message not found' });
-//         }
-//         res.json({ message: 'Message deleted successfully' });
-//     });
-// });
-
-
 app.listen(PORT, ()=>{
-    console.log(`Servidor rodando na porta 3000`);
+    console.log(process.env)
+    console.log(`Servidor rodando na porta ${PORT}`);
 });
