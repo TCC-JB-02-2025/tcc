cf1257857cbf0cbb2443800ba73ba5497832a9a2
Renan Andrade <77841497+Laggh@users.noreply.github.com>
2025-04-29 20:27:12 -0300

    triggers do user + searchIndex

--- server/migrations/004_insert_into_users.sql
+++ server/migrations/004_insert_into_users.sql
@@ -1,23 +1,8 @@
 -- Down
-DROP TABLE IF EXISTS User; 
+-- Deleta todos os usuários da tabela User
+DELETE FROM User;
 
 -- Up
-CREATE TABLE User (
-    user_id INT AUTO_INCREMENT PRIMARY KEY,
-    full_name VARCHAR(255) NOT NULL,
-    cpf VARCHAR(11) UNIQUE NOT NULL,
-    email VARCHAR(255) UNIQUE NOT NULL,
-    password_hash VARCHAR(255) UNIQUE NOT NULL,
-    address_street VARCHAR(255) NOT NULL,
-    address_number VARCHAR(255) NOT NULL,
-    address_complement VARCHAR(255), 
-    address_city VARCHAR(255) NOT NULL, 
-    cep VARCHAR(8) NOT NULL, 
-    notifications_json JSON DEFAULT '{}',
-    settings_json JSON DEFAULT '{}',
-    creation_timestamp TIMESTAMP DEFAULT CURRENT_TIMESTAMP
-);
-
 INSERT INTO User (
     full_name,
     cpf,
