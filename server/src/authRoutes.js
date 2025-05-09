bf0c668efd6a687c5c76e177df4b7126149224cc
Renan Andrade <77841497+Laggh@users.noreply.github.com>
2025-05-09 18:12:50 -0300

    hash implementado

--- server/src/authRoutes.js
+++ server/src/authRoutes.js
@@ -1,6 +1,11 @@
 const express = require('express');
 const { validateCPF } = require("./helpers");
 
+// Importa o bcrypt para fazer o hash da senha
+const bcrypt = require('bcrypt');
+const saltRounds = 10; // quantos calculos de hash serão feitos
+const bcryptSalt = bcrypt.genSaltSync(saltRounds);
+
 module.exports = (pool) => {
   const router = express.Router();
 
@@ -22,18 +27,21 @@ module.exports = (pool) => {
       cep
     };
     // Adiciona o hash da senha
-    user.password_hash = user.password;
-  
-    user.password = undefined;
+    user.password_hash = bcrypt.hashSync(user.password, bcryptSalt);
+    
+    // Apaga a senha do objeto antes de enviar para o banco de dados
+    // Isso é importante para não enviar a senha em texto claro pro DB
+    delete user.password; // não pode usar password=undifined, pq se não da erro
 
     // Passa por cada uma das chaves obrigatorias e verifica se existe
     const requiredFields = ['full_name', 'cpf', 'email', 'password_hash', 'address_street', 'address_number', 'address_city', 'cep'];
 
     for (const field of requiredFields) {
-      if (!user[field]) {
+      if (!user[field]) { // se não existe da erro
         return res.status(400).json({ error: `Missing required field: ${field}`, request: req.body });
       }
     }
+
 
     // Verifica se o CPF é do tamanho certo e se é valido (usando a formula do CPF)
     if (!validateCPF(user.cpf)) {
